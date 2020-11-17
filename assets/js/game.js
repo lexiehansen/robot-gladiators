757416429c77542056d094449348ecf2d6f7b599
Lexie Hansen <home@Lexies-MacBook-Pro.local>
2020-11-17 12:41:49 -0700

    add while loop to robot name prompt

--- assets/js/game.js
+++ assets/js/game.js
@@ -63,8 +63,20 @@ var randomNumber = function(min, max) {
   return value;
 };
   
+// function to set name
+var getPlayerName = function() {
+  var name = "";
+
+  while (name === "" || name === null) {
+    name = prompt("What is your robot's name?");
+  }
+
+  console.log(""Your robot's name is "" + name);
+  return name;
+};
+
 var playerInfo = {
-  name: window.prompt("What is your robot's name?"),
+  name: getPlayerName(),
   health: 100,
   attack: 10,
   money: 10,
@@ -120,7 +132,7 @@ var startGame = function() {
         }
     }
     // play again
-    startGame();
+    //startGame();
 
       // after the loop ends, player is either out of health or enemies to fight, so run the endGame function
     endGame();
