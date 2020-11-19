32b4adcc9ebd02e7cf1f5e7c243ea940202f673d
Lexie Hansen <home@Lexies-MacBook-Pro.local>
2020-11-18 17:45:06 -0700

    resolve fight skip bug

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,8 +1,43 @@
+var fightOrSkip = function() {
+  // ask player if they'd like to fight or skip using fightOrSkip function
+  var promptFight = window.prompt('Would you like FIGHT or SKIP this battle? Enter "FIGHT" or "SKIP" to choose.');
+
+  // Conditional Recursive Function Call
+  if (promptFight === "" || promptFight === null) {
+    window.alert("You need to provide a valid answer! Please try again.");
+    return fightOrSkip();
+  }
+
+  promptFight = promptFight.toLowerCase();
+
+  // if player picks "skip" confirm and then stop the loop
+  if (promptFight === "skip" || promptFight === "SKIP") {
+    // confirm player wants to skip
+    var confirmSkip = window.confirm("Are you sure you'd like to quit?");
+
+    // if yes (true), leave fight
+    if (confirmSkip) {
+      window.alert(playerInfo.name + " has decided to skip this fight. Goodbye!");
+      // subtract money from playerMoney for skipping, but don't let them go into the negative
+      playerInfo.money = Math.max(0, playerInfo.money - 10);
+
+      // return true if player wants to leave
+      return true;
+    }
+  }
+}
+
 var fight = function(enemy) {
-    while (playerInfo.health > 0 && enemy.health > 0) {
-      // ask player if they'd liked to fight or run
-      var promptFight = window.prompt('Would you like FIGHT or SKIP this battle? Enter "FIGHT" or "SKIP" to choose.');
-  
+    // repeat and execute as long as the enemy-robot is alive 
+    while(enemy.health > 0 && playerInfo.health > 0) {
+      // ask player if they'd like to fight or skip using fightOrSkip function
+      if (fightOrSkip()) {
+        // if true, leave fight by breaking loop
+        break;
+      }
+
+      var damage = randomNumber(playerInfo.attack - 3, playerInfo.attack);
+
       // if player picks "skip" confirm and then stop the loop
       if (promptFight === "skip" || promptFight === "SKIP") {
         // confirm player wants to skip
@@ -71,7 +106,7 @@ var getPlayerName = function() {
     name = prompt("What is your robot's name?");
   }
 
-  console.log(""Your robot's name is "" + name);
+  console.log("Your robot's name is " + name);
   return name;
 };
 
